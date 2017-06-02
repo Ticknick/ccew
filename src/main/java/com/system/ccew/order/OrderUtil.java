01bad75fb32f5e2f57b11f8caca12dc055838c8d
Ticknick <ticknick.hou@gmail.com>
2017-06-02 21:57:51 +0800

    1

--- src/main/java/com/system/ccew/order/OrderUtil.java
+++ src/main/java/com/system/ccew/order/OrderUtil.java
@@ -1,11 +1,7 @@
 package com.system.ccew.order;
 
-import com.delivery.common.constant.Constant;
-import com.delivery.common.dao.CreditRecordDao;
-import com.delivery.common.dao.OrderDao;
-import com.delivery.common.entity.CreditRecordEntity;
-import com.delivery.common.entity.OrderEntity;
-import com.delivery.common.entity.UserEntity;
+import com.system.ccew.dao.OrderDao;
+import com.system.ccew.entity.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -18,40 +14,27 @@ import java.util.List;
 @Component
 public class OrderUtil {
 
-    @Autowired
-    CreditRecordDao creditRecordDao;
 
     @Autowired
     OrderDao orderDao;
 
-    public boolean canCreate(UserEntity user) {
-        if (getCreditValue(user) < Constant.USER_CAN_CREATE_ORDER_CREDIT_LIMIT) {
-            return false;
-        }
-        int runingOrderCount = orderDao.findByReplacementId(user.getUid()).size();
-        if (runingOrderCount > Constant.USER_CREATE_ORDER_MAX_COUNT) {
-            return false;
-        }
-
-        int complaintCount = orderDao.findByIdAndState(user.getUid(), OrderEntity.OrderState.COMPLAINING).size();
-        if (complaintCount != 0) {
-            return false;
-        }
-
-        return true;
-    }
-
-    public int getCreditValue(UserEntity user) {
-        List<CreditRecordEntity> records = creditRecordDao.findByUserId(user.getUid());
-        int value = 0;
-        for (CreditRecordEntity record : records) {
-            value += record.getValue();
-        }
-        return value;
-    }
+//    public boolean canCreate(UserEntity user) {
+//        if (getCreditValue(user) < Constant.USER_CAN_CREATE_ORDER_CREDIT_LIMIT) {
+//            return false;
+//        }
+//        int runingOrderCount = orderDao.findByReplacementId(user.getUid()).size();
+//        if (runingOrderCount > Constant.USER_CREATE_ORDER_MAX_COUNT) {
+//            return false;
+//        }
+//
+//        int complaintCount = orderDao.findByIdAndState(user.getUid(), OrderEntity.OrderState.COMPLAINING).size();
+//        if (complaintCount != 0) {
+//            return false;
+//        }
+//
+//        return true;
+//    }
 
 
-    public boolean isParticipation(UserEntity user, OrderEntity order) {
-        return user.getUid().equals(order.getRecipientId()) || user.getUid().equals(order.getReplacementId());
-    }
+
 }
