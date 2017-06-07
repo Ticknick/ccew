3a571c37bcd43c8e79a4200415f57842482bb00d
Stevenljl <2234064569@qq.com>
2017-06-07 14:12:44 +0800

    a

--- push.sh
+++ push.sh
@@ -1,6 +1,30 @@
 #!/bin/sh
 git add .
+
+if [ $? -eq 0 ]
+then echo ok
+else
+    echo error
+    exit 1
+fi
+
 git commit -m a
+
+if [ $? -eq 0 ]
+then echo ok
+else
+    echo error
+    exit 1
+fi
+
 git push
+
+if [ $? -eq 0 ]
+then echo ok
+else
+    echo error
+    exit 1
+fi
+
 echo push ok
 ssh root@ticknick.me ./caffeine_deploy
