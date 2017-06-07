7a7b55c991dbcd17360030d1cfb46cb1dba881d5
Ticknick <ticknick.hou@gmail.com>
2017-06-07 10:56:02 +0800

    a

--- src/main/java/com/system/ccew/entity/OrderState.java
+++ src/main/java/com/system/ccew/entity/OrderState.java
@@ -5,5 +5,5 @@ package com.system.ccew.entity;
  * @date 02/06/2017
  */
 public enum  OrderState {
-        WAIT,ACCEPT,SUCCESS,FAIL
+        WAIT,ACCEPT,SUCCESS,FAILED1,FAILED2,FAILED3,FAILED4,FAILED5,FAILED;
 }
