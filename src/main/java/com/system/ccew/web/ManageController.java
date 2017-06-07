899e2cb30a9eb6ee7e0de778a45578455d5c77ad
Stevenljl <2234064569@qq.com>
2017-06-07 15:31:28 +0800

    a

--- src/main/java/com/system/ccew/web/ManageController.java
+++ src/main/java/com/system/ccew/web/ManageController.java
@@ -16,18 +16,18 @@ public class ManageController {
         return "caffeine/examine";
     }
 
-    @RequestMapping("/caffeine/complaints")
-    public String hello2() {
-        return "caffeine/complaints";
-    }
-    @RequestMapping("/caffeine/new")
-    public String hello3() {
-        return "caffeine/new";
-    }
-    @RequestMapping("/caffeine/query")
-    public String hello4() {
-        return "caffeine/query";
-    }
+//    @RequestMapping("/caffeine/complaints")
+//    public String hello2() {
+//        return "caffeine/complaints";
+//    }
+//    @RequestMapping("/caffeine/new")
+//    public String hello3() {
+//        return "caffeine/new";
+//    }
+//    @RequestMapping("/caffeine/query")
+//    public String hello4() {
+//        return "caffeine/query";
+//    }
     @RequestMapping("/")
     public String hello5() {
         return "caffeine/signin";
